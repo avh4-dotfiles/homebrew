750b87f6bf72854b2b8529abbf6663f2d7dfca71
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-12-07 13:04:39 -0500

    Add mvn, sbt to check_commands

--- .bash_profile_includes/check_commands.sh
+++ .bash_profile_includes/check_commands.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in wget git curl gdate mate; do
+for i in wget git curl gdate mate mvn sbt; do
 	if ! which "$i" >/dev/null; then
 		echo "$BASH_SOURCE: Command not found on PATH: $i" >&2
 	fi
